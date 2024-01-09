459b41353edccfbabb80b1a36a843e4cfdf28d3b
Bea Obiero <bea.obiero@gmail.comgit config --global user.email bea.obiero@gmail.comgit config --global user.email + + bea.obiero@gmail.com>
2024-01-09 17:53:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -12,5 +12,19 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  //iterate each tutorial
+  return tutorials.map(tutorial => {
+    //split tutorial into  array
+    const words = tutorial.split("");
+
+    //capitalise first letter of each word using .map
+    const titleCasedWords = words.map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1)//rest of the word is lower case;
+    })
+
+    //bring back title case words back together into a string
+    return titleCasedWords.join("");
+  })
 }
+
+console.log(titleCased());
